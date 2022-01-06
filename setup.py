ecfeb0c671ffad1377b81bf8f4f4ab9c550dc7dd
Théis Bazin <tbazin@users.noreply.github.com>
2022-01-06 19:09:12 +0100

    :memo: Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='GANsynth_pytorch',
-      version='0.1',
+      version='1.0.0',
       url='https://github.com/SonyCSLParis/GANsynth_pytorch',
       author=['Theis Bazin', 'ss12f32v'],
       author_email=['tbazin@github.com', 'cloudstrife60138@gmail.com'],
